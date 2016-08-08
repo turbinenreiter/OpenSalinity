d72c5b57e8d2f2348c30fc73a078217f6b6e72ca
Sebastian Plamauer <oeplse@gmail.com>
2016-08-08 15:36:56 +0200

    almost there

--- src/vis/vis.py
+++ src/vis/vis.py
@@ -27,7 +27,10 @@ def roll_max_savgol(vector):
         except:
             pass
     off = [0]*100
-    return sps.savgol_filter(np.insert(vector, 1, off)[:-100], 99, 2)
+    global vemf
+    vemf = copy.copy(vector)
+    vemf = np.insert(vemf, 1, off)[:-100]
+    return sps.savgol_filter(np.insert(vector, 1, off)[:-100], 333, 2)
 
 def main(argv):
 
@@ -35,7 +38,7 @@ def main(argv):
 
     fname = argv[1]
 
-    start = 0#3000
+    start = 0
     length = None
     te1, e1, te2, e2, te3, e3, te4, e4, te5, e5, te6, e6, te7, e7, te8, e8 = np.genfromtxt(fname, delimiter=' ', skip_header=start, max_rows=length, unpack=True)
 
@@ -93,7 +96,7 @@ def main(argv):
     # plot
 
     #     0   1   2   3   4   5   6   7
-    c = ['b','c','w','g','y','m','b','r']
+    c = ['b','b','b','b','g','g','g','r']
 
     fig, ax = plt.subplots() #plt.subplots(nrows=2, ncols=1, sharex=True)
 
@@ -103,12 +106,15 @@ def main(argv):
     tau = np.mean(t[0]-t[1])
 
     for i, (vt, ve, ve_raw, vfreq, vd) in enumerate(zip(t, e, e_raw, freqs, diff)):
-        if i in [0,3,4,7]:
+        if i in [3,7]: #0,1,2,3,4,5,6,7
 
+            s = 2000
+            e = -50000
             # sal
             #plt.subplot(211)
-            ax.plot(vt[2:], ve[2:], alpha=0.75, label='#'+str(i+1), color=c[i])
-            #ax.plot(vt[2:], ve_raw[2:], alpha=0.25, label='#'+str(i+1)+'r', color=c[i])
+            ax.plot(vt[s:e], ve[s:e], alpha=0.75, label='#'+str(i+1), color=c[i], linewidth=1.5)
+            #ax.plot(vt[s:e], vemf[s:e], alpha=0.50, label='#'+str(i+1), color='b', linewidth=1.5)
+            #ax.scatter(vt[s:e], ve_raw[s:e], alpha=0.25, label='#'+str(i+1)+'raw', color=c[i], marker='.')
 
             lfont = {'fontname':'Source Serif Pro'}
             ax.set_xlabel('Time, s', **lfont)
@@ -116,7 +122,7 @@ def main(argv):
             #ax.legend(loc='best')
 
             ax.set_ylim([0,1])
-            ax.set_xlim([0,vt[2:][-1]])
+            ax.set_xlim([vt[s],vt[e]])
             ax.grid(True, linestyle='dashed')
             # diff
             #plt.subplot(212)
@@ -129,13 +135,13 @@ def main(argv):
             #plt.plot(vfreq, 20*sp.log10(abs(sp.fft(ve_raw))),
             #         '.', alpha=0.6, color=c[i])
 
-#    try:
-    if argv[2] == '-s':
-        plt.savefig(fname.split('.')[0]+'.pdf', dpi=300)
-        from matplotlib2tikz import save as tikz_save
-        tikz_save(fname.split('.')[0]+'.tex')
-#    except:
-    else:
+    try:
+        if argv[2] == '-s':
+            plt.savefig(fname.split('.')[0]+'.pdf', dpi=300)
+            from matplotlib2tikz import save as tikz_save
+            tikz_save(fname.split('.')[0]+'.tex')
+    except:
+#    else:
         plt.show()
         #mpld3.save_html(fig, 'plot.html')
 
