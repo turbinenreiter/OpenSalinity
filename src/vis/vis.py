7b94da0d598f47edd8aeb664404aaade8ba67700
Sebastian Plamauer <oeplse@gmail.com>
2016-08-09 19:01:15 +0200

    more fixes

--- src/vis/vis.py
+++ src/vis/vis.py
@@ -3,6 +3,7 @@
 import sys, getopt
 
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 import copy
 #import mpld3
 import numpy as np
@@ -41,6 +42,8 @@ def main(argv):
     start = 0
     length = None
     te1, e1, te2, e2, te3, e3, te4, e4, te5, e5, te6, e6, te7, e7, te8, e8 = np.genfromtxt(fname, delimiter=' ', skip_header=start, max_rows=length, unpack=True)
+
+    sim = np.genfromtxt('/home/plam/Documents/Semesterarbeit/OpenSalinity/src/sim_comp/data/1.6/simulationData.csv', names=True, delimiter=';', skip_header=4)
 
     print('data read')
 
@@ -96,34 +99,60 @@ def main(argv):
     # plot
 
     #     0   1   2   3   4   5   6   7
-    c = ['b','b','b','b','g','g','g','r']
+    c = ['b','y','k','c','m','g','g','r']
 
-    fig, ax = plt.subplots() #plt.subplots(nrows=2, ncols=1, sharex=True)
+    fig, axg = plt.subplots() #plt.subplots(nrows=2, ncols=1, sharex=True)
 
     #fig.suptitle(fname.split('/')[-1])
     #plt.style.use('bmh')
 
     tau = np.mean(t[0]-t[1])
 
-    for i, (vt, ve, ve_raw, vfreq, vd) in enumerate(zip(t, e, e_raw, freqs, diff)):
-        if i in [3,7]: #0,1,2,3,4,5,6,7
+    grid = gs.GridSpec(3,2)
+    sp = [0]*8
+    count = 0
 
-            s = 2000
-            e = -50000
+    for i, (vt, ve, ve_raw, vfreq, vd) in enumerate(zip(t, e, e_raw, freqs, diff)):
+        if i in [0,1,3,4,5,7]: #0,1,2,3,4,5,6,7
+
+            sp[count] = fig.add_subplot(grid[count])
+            ax = sp[count]
+            count = count + 1
+
+            s = 10000
+            e = -28000
             # sal
             #plt.subplot(211)
-            ax.plot(vt[s:e], ve[s:e], alpha=0.75, label='#'+str(i+1), color=c[i], linewidth=1.5)
+            ax.plot(vt[s:e], ve[s:e], alpha=0.75, label='#'+str(i+1),
+                    color=c[i], linewidth=1.5)
             #ax.plot(vt[s:e], vemf[s:e], alpha=0.50, label='#'+str(i+1), color='b', linewidth=1.5)
-            #ax.scatter(vt[s:e], ve_raw[s:e], alpha=0.25, label='#'+str(i+1)+'raw', color=c[i], marker='.')
+            #ax.scatter(vt[s:e], ve_raw[s:e], alpha=0.25,
+            #           label='#'+str(i+1)+'raw', color=c[i], marker='.')
 
-            lfont = {'fontname':'Source Serif Pro'}
-            ax.set_xlabel('Time, s', **lfont)
-            ax.set_ylabel('Voltage, V', **lfont)
-            #ax.legend(loc='best')
+            dt = +66.5+2.5
+            sc = max(ve)
+            ax.scatter(sim['Time']+dt, sim['Sensor_'+str(i+1)]*sc,
+                       alpha=0.25, color=c[i], marker='.')
+
+            ax.set_yticks([0.0,0.3,0.6,0.9])
+
+            if count%2 == 0:
+                ax.set_yticklabels([])
+            if count < 6-2:
+                ax.set_xticklabels([])
 
             ax.set_ylim([0,1])
             ax.set_xlim([vt[s],vt[e]])
             ax.grid(True, linestyle='dashed')
+
+            lfont = {'fontname':'Source Serif Pro'}
+            axg.set_xlabel('Time, s', **lfont, labelpad=24)
+            axg.set_ylabel('Voltage, V', **lfont, labelpad=32)
+            axg.set_yticks([])
+            axg.set_xticks([])
+            #axg.xaxis.set_visible(False)
+            #axg.yaxis.set_visible(False)
+
             # diff
             #plt.subplot(212)
             #ax[1].plot(vt[2:], vd[0], alpha=0.5, label='d#'+str(i+1), color=c[i])
@@ -134,6 +163,8 @@ def main(argv):
             #plt.subplot(212)
             #plt.plot(vfreq, 20*sp.log10(abs(sp.fft(ve_raw))),
             #         '.', alpha=0.6, color=c[i])
+
+    fig.tight_layout()
 
     try:
         if argv[2] == '-s':
