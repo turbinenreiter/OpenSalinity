863a5e3a765be0036308750a2b3abb2709932820
Sebastian Plamauer <oeplse@gmail.com>
2017-03-07 17:29:45 +0100

    Add smart I2C device detection to avoid errors when switching ADCs

--- src/OpenSalintiyFirmware/main.py
+++ src/OpenSalintiyFirmware/main.py
@@ -1,16 +1,14 @@
 import pyb
 from SalSense8 import SalSens8
 
-if __name__ == "__main__":
+pyb.LED(2).on()
+sal_sens = SalSens8(i2c=pyb.I2C(1, pyb.I2C.MASTER),
+                    mec_addr=pyb.I2C(1, pyb.I2C.MASTER).scan()[0])
 
-    pyb.LED(2).on()
+def loop(delay=100):
+    start = pyb.micros()
+    while True:
+        sal_sens.pretty_print(start)
+        pyb.delay(delay)
 
-    sal_sens = SalSens8(i2c=pyb.I2C(1, pyb.I2C.MASTER), mec_addr=77)
-
-    def loop(delay=100):
-        start = pyb.micros()
-        while True:
-            sal_sens.pretty_print(start)
-            pyb.delay(delay)
-
-    loop(delay=0)
+loop(delay=0)
